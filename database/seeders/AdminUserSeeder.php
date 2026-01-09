d030d6ba051833abbebdd3846973f4e2fc78d9d0
martoast <alexmartos96@gmail.com>
2026-01-09 13:28:20 -0800

    update seeder

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -11,7 +11,7 @@ class AdminUserSeeder extends Seeder
     public function run(): void
     {
         User::updateOrCreate(
-            ['email' => 'admin@evenly.com'],
+            ['email' => 'admin@mesadirectiva.com'],
             [
                 'name' => 'Super Admin',
                 'password' => Hash::make('password'),
@@ -21,6 +21,6 @@ class AdminUserSeeder extends Seeder
             ]
         );
 
-        $this->command->info('Admin user created: admin@evenly.com / password');
+        $this->command->info('Admin user created: admin@mesadirectiva.com / password');
     }
 }
